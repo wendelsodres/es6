25df851d0d114cb5c078b08c2d1e60508e708a74
Wendel Sodré <wendelsodres@gmail.com>
2019-08-30 07:49:23 -0300

    teste not twig2

--- web/index.php
+++ web/index.php
@@ -2,14 +2,5 @@
 
 require('../vendor/autoload.php');
 
-$app = new Silex\Application();
-$app['debug'] = true;
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
 // Register view rendering
 require('views/.index.html');
-$app->run();
