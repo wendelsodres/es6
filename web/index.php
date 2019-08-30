76dd1961a184f1571cde83efc34cc3cce9c9e654
Wendel Sodré <wendelsodres@gmail.com>
2019-08-30 07:40:48 -0300

    Test view.

--- web/index.php
+++ web/index.php
@@ -15,11 +15,4 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
 
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
-
 $app->run();
